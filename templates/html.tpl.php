5a2ac232d43815c9ed8d5d50ae3fe62794ec310d
Søren Due Bonde <sdb@novicell.dk>
2023-10-11 08:59:26 +0200

    OS2ULF-655: Adjusted newly added monsido scripts

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -49,7 +49,7 @@
     <title><?php print $head_title; ?></title>
     <?php print $styles; ?>
     <?php print $scripts; ?>
-    <script src=https://ugtracker.hjoerring.dk/tracker.js></script>
+    <script async src=https://ugtracker.hjoerring.dk/tracker.js></script>
     <meta http-equiv="X-UA-Compatible" content="IE=9">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"/>
     <!--[if lt IE 10]>
